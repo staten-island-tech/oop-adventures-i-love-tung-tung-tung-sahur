87ddf00636242f1cdfd6b87a80f03ab35acf390c
ansontansiths <ansont5@nycstudents.net>
2026-05-04 10:13:24 -0400

    commit

---
+++ tungtung.py
@@ -0,0 +1,6 @@
+class tung:
+    def __init__(self,health,aura,morale,attackpower):
+        self.health = health
+        self.aura = aura
+        self.morale = morale
+        self.attackpower = attackpower
