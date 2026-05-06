a8ca74825a1e690afc1cd3e1a1744cc5523d4181
ansontansiths <ansont5@nycstudents.net>
2026-05-06 09:51:47 -0400

    commit

--- tungtung.py
+++ tungtung.py
@@ -4,3 +4,18 @@ class tung:
         self.aura = aura
         self.morale = morale
         self.attackpower = attackpower
+    def beginnings():
+        answer = input("wake up?")
+        answer = answer.lower
+        if answer == "yes":
+            print("welcome big triple t")
+        
+        elif answer == "no":
+            print("game over")
+        return
+
+    beginnings()
+
+
+    
+        
