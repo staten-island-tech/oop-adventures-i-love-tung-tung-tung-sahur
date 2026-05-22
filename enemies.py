0189de09683d28627a82554a53f5b23b5e275f8f
ansontansiths <ansont5@nycstudents.net>
2026-05-22 10:02:14 -0400

    commit

---
+++ enemies.py
@@ -0,0 +1,35 @@
+import random
+
+class enemy:
+        def __init__(self,enemhealth,enemattack,lootedaura):
+            self.enemhealth = enemhealth
+            self.enemattack = enemattack
+            self.lootedaura = lootedaura
+
+eviltungtung = enemy(40,10,2)
+evilrabiesdog = enemy(25,20,3)
+mutatedsupertung = enemy(60,10,2)
+Enemies = [evilrabiesdog,eviltungtung,mutatedsupertung]
+def enemyencounter():
+    enemysearch = input('would you like to search for enemies?').lower
+    if enemysearch != "no":
+        print("ok searching for enemies")
+        enemyfound = random.choice(Enemies)
+        if enemyfound == eviltungtung:
+            print("you have encountered a eviltungtung!")
+        elif enemyfound == evilrabiesdog:
+            print("you have found a evilrabiesdog!")
+        elif enemyfound == mutatedsupertung:
+            print("you have found a mutatedsupertung")
+    else:
+        print("too bad")
+        if enemyfound == eviltungtung:
+            print("you have encountered a eviltungtung!")
+        elif enemyfound == evilrabiesdog:
+            print("you have found a evilrabiesdog!")
+        elif enemyfound == mutatedsupertung:
+            print("you have found a mutatedsupertung")
+    
+
+
+enemyencounter()
--- tungtung.py
+++ tungtung.py
@@ -1,20 +1,3 @@
-tungtunggodrelic = {
-        "name":"tungtunggodrelic",
-        "cost": 10,
-        
-}
-tungtunggodbat = {
-        "name":"tungtunggodbat",
-        "cost": 10,
-        
-}
-
-spraythatkillseverything = {
-        "name":"spraythatkillseverything",
-}
-sonion = {
-        "name":"sonion",
-}
 
 
 
@@ -27,53 +10,28 @@ sonion = {
 
 
 
-
-
-Boss_drops = [spraythatkillseverything,sonion]
-Purchasables = [tungtunggodrelic,tungtunggodbat]
-class debtmafia:
-    def __init__(self,mafiahealth,mafiaattack,debttakenoff):
-        self.mafiahealth = mafiahealth
-        self.mafiaattack = mafiaattack
-        self.debttakenoff = debttakenoff
-class boss:
-    def __init__(self,bosshealth,bossattack,bosslootedaura):
-        self.bosshealth = bosshealth
-        self.bossattack = bossattack
-        self.bosslootedaura = bosslootedaura
-
-class enemy:
-    def __init__(self,enemhealth,enemattack,lootedaura):
-        self.enemhealth = enemhealth
-        self.enemattack = enemattack
-        self.lootedaura = lootedaura
 class tung:
-    def __init__(self,health,aura,morale,attackpower):
+    def __init__(self,health,aura,attackpower):
         self.health = health
         self.aura = aura    #currency
-        self.morale = morale    #percent increase/decrease multiplier to attackpower
         self.attackpower = attackpower
     def beginnings(self,answer):
-        self.answer=answer
-        answer = input("wake up?").lower()
-        if answer != "no":
-            print("welcome big triple t")
-        else:
-            print("game over")
-    def shop(self, Purchasables,attackpower,aura,health,TripleT):
-        shopask = input("would you like to shop?").lower()
-        if shopask != "no":
-            print(Purchasables,attackpower,aura,health,TripleT)
-            shopbuy = input("what would you like to purchase").lower()
-            if shopbuy == "tungtunggodrelic" :
-                print('bought item!')
-                self.health += 50
-            
+            self.answer=answer
+            answer = input("wake up?").lower()
+            if answer != "no":
+                print("welcome big triple t")
+            else:
+                ("game over")
+
         
 
-            
 
-    shop(Purchasables) 
-TripleT=tung(100,10,0,1)
+            
+        
+            
+TripleT=tung(100,10,1)
 TripleT.beginnings("")
     
+
+    
+            
