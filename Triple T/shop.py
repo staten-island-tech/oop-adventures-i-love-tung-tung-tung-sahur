26b55b85291b7f19d6e2f95ef5b40cfacf2c83b6
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-06-01 10:17:17 -0400

    commit

--- Triple T/shop.py
+++ Triple T/shop.py
@@ -14,27 +14,28 @@ Purchasables = [tungtunggodrelic,tungtunggodbat]
 
 
 def shop(self, Purchasables,attackpower,aura,health):
-        shopask = input("would you like to shop?").lower()
-        if shopask != "no":
-            print(Purchasables,attackpower,aura,health,)
-            shopbuy = input("what would you like to purchase").lower()
-            if shopbuy == "tungtunggodrelic" :
-                print('bought item!')
-                self.health += 50
-                self.aura -= 10
-                self.attackpower += 5
-                
-            if shopbuy == "tungtunggodbat":
-                print('bought item!')
-                self.aura -= 10
-                self.attackpower += 20
+    shopask = input("would you like to shop? ").lower()
+    if shopask != "no":
+        print(Purchasables,attackpower,aura,health,)
+        shopbuy = input("what would you like to purchase? ").lower()
+        if shopbuy == "tungtunggodrelic" :
+            print('bought item!')
+            self.health += 50
+            self.aura -= 10
+            self.attackpower += 5
             
-            else: 
-                print("im sorry we dont have that item in stock")
-            
-            if aura < 0:
-                print("you are now in aura debt")
-            
-            elif aura == 0:
-                print("you are now broke") 
+        if shopbuy == "tungtunggodbat":
+            print('bought item!')
+            self.aura -= 10
+            self.attackpower += 20
+        
+        else: 
+            print("im sorry we dont have that item in stock")
+        
+        if aura < 0:
+            print("you are now in aura debt")
+        
+        elif aura == 0:
+            print("you are now broke")
+    print(self.__dict__)
 
--- Triple T/tungtung.py
+++ Triple T/tungtung.py
@@ -8,13 +8,18 @@ class tung:
         self.health = health
         self.aura = aura    #currency
         self.attackpower = attackpower
-    def beginnings():
+    def beginnings(self):
         return ugui.beninnins()
+    def shopi(self,Purchasables,attackpower,aura,health):
+        return shop.shop(self,Purchasables,attackpower,aura,health)
+    def enemi(self):
+        return enemies.enenimy()
+    
 #enemies.Enemies      
 TripleT=tung(100,10,1)
 TripleT.beginnings()
-TripleT.shop(shop.Purchasables,TripleT.attackpower,TripleT.aura,TripleT.health)
-TripleT.enemyencounter()
+TripleT.shopi(shop.Purchasables,TripleT.attackpower,TripleT.aura,TripleT.health)
+TripleT.enemi()
 
     
             
