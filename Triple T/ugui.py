cc2a7e423120e9c41e233d4c24e287bdbfa5407d
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-06-11 10:17:09 -0400

    commit

--- Triple T/ugui.py
+++ Triple T/ugui.py
@@ -11,19 +11,20 @@ def beninnings():
     # Create the main application window
     root = tk.Tk()
     root.title('tungtungtung sahur')
-    root.geometry('200x150')  # Width x Height
+    root.geometry('100x150')  # Width x Height
 
     # Create a label
     label = tk.Label(root, text="Wake up?", font=("Sitka Display", 12))
     label.pack(pady=10)
 
     # Create a text entry field
-    name_entry = tk.Entry(root, font=("Mongolian Baiti", 12))
-    name_entry.pack(pady=5)
+    
 
     # Create a button
-    greet_button = tk.Button(root, text="Answer", font=("Times New Roman", 12), command=beninnins)
-    greet_button.pack(pady=10)
+    yes = tk.Button(root, text="Yes", font=("Times New Roman", 12), command=beninnins)
+    yes.pack(pady=10)
+    no= tk.Button(root, text="No", font=("Times New Roman", 12), command=begiggigs)
+    no.pack(pady=10)
 
     # Start the Tkinter event loop
     root.mainloop()
