c390a8f251e95bb79c10b38b68282f66cbd82223
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-06-16 10:01:57 -0400

    commit

--- Triple T/fight.py
+++ Triple T/fight.py
@@ -1,33 +1,43 @@
 import tungtung
 import enemies
 from enemies import enemyencounter
-def battle():  
+
+
+def battle():
     enemy = enemyencounter()
-    if enemy is not None and tungtung.TripleT.health > 0:
-        if enemy == enemies.eviltungtung:
-            print("eviltungtung looks at you with sinister intent")
-        if enemy == enemies.evilrabiesdog:
-            print("evilrabiesdog drools")
-        if enemy == enemies.mutatedsupertung:
-            print("mutatedsupertung flexes his arms")
+    if enemy is None:
+        print("no enemy found")
+        return
+
+
     playerhealth = tungtung.TripleT.health
     playerdamage = tungtung.TripleT.attackpower
     enemyhealth = enemy.enemhealth
     enemyattack = enemy.enemattack
-    while tungtung.TripleT.health > 0 and enemy.enemhealth > 0:
+
+
+    while playerhealth > 0 and enemyhealth > 0:
         print("you are now in battle!")
-        print(f"your health is{playerhealth}and enemies health is{enemyhealth}")
-   
-        action = input("what action would you like to do? (attack,defend)")
-        action = action.lower
+        print(f"your health is {playerhealth} and enemies health is {enemyhealth}")
+        action = input("what action would you like to do? (attack, defend)").lower()
 
 
         if action == "attack":
-            enemyhealth - playerdamage
-        print(enemyhealth)
-        if action == "defend":
+            enemyhealth -= playerdamage
+            playerhealth -= enemyattack
+        elif action == "defend":
             playerhealth -= enemyattack - 5
+        else:
+            print("invalid action u do nothing nerd")
+            playerhealth -= enemyattack
+
+
         if enemyhealth <= 0:
             tungtung.TripleT.aura += enemy.lootedaura
-            print("you win!")    
-            break        
+            print("you win!")
+            break
+        if playerhealth <= 0:
+            print("you died rip")
+            break
+
+
--- Triple T/tungtung.py
+++ Triple T/tungtung.py
@@ -1,24 +1,61 @@
-import ugui
-import shop
-import enemies
-#import fight
-import random
+tungtunggodrelic = {
+        "name":"tungtunggodrelic",
+        "cost": 10,
+        'health': 50,
+        'attack': 5
+       
+}
+tungtunggodbat = {
+        "name":"tungtunggodbat",
+        "cost": 10,
+        'attack': 20
+       
+}
+
+
+Purchasables = [tungtunggodrelic,tungtunggodbat]
+
+
+
+
+
 
 class tung:
     def __init__(self,health,aura,attackpower):
         self.health = health
         self.aura = aura    #currency
         self.attackpower = attackpower
-    def beginnings(self):
-        return ugui.beninnings()
-    def shopi(self,Purchasables,attackpower,aura,health):
-        return shop.shop(self,Purchasables,attackpower,aura,health)
-    def enemi(self):
-        return enemies.enemyencounter()
-
-#enemies.Enemies      
+    def shop(self, Purchasables,attackpower,aura,health):
+        shopask = input("would you like to shop?").lower()
+        if shopask != "no":
+            print(Purchasables,attackpower,aura,health,)
+            shopbuy = input("what would you like to purchase").lower()
+            if shopbuy == "tungtunggodrelic" :
+                print('bought item!')
+                self.aura -= Purchasables[0]['cost']
+                self.health += Purchasables[0]['health']
+                self.attackpower += Purchasables[0]['attack']
+               
+            if shopbuy == "tungtunggodbat":
+                print('bought item!')
+                self.aura -= 10
+                self.attackpower += 20
+           
+            else:
+                print("im sorry we dont have that item in stock")
+           
+            if aura < 0:
+                print("you are now in aura debt")
+           
+            elif aura == 0:
+                print("you are now broke")
+            print(self.__dict__)
+    def beginnings(self,answer):
+            self.answer=answer
+            answer = input("wake up?").lower()
+            if answer != "no":
+                print("welcome big triple t")
+            else:
+                ("game over")
+           
 TripleT=tung(100,10,1)
-TripleT.beginnings()
-TripleT.shopi(shop.Purchasables,TripleT.attackpower,TripleT.aura,TripleT.health)
-TripleT.enemi()
-#battle(TripleT,enemies.Enemies[enemies.Enemies[1]])
